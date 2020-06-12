dc4b1e9a5b57c357fa6d5939b5bb470ea2766fe7
Mariamol James <mariamol.james@gmail.com>
2020-06-12 18:19:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,7 @@ def load_library(path)
   emoticons_hash={}
   count=0
   while count<emoticons.length do
+<<<<<<< HEAD
     emoticons_hash[emoticons.keys[count]]={}
     count+=1
   end
@@ -16,6 +17,12 @@ def load_library(path)
     emoticons_hash[emoticons.keys[count]][:japanese]=emoticons.values[count][1]
     count+=1
   end
+=======
+    emoticons_hash[:emoticons[count]]={}
+    count+=1
+  end
+  binding.pry
+>>>>>>> 732e70a4eb0a84688681643024051991d19fd193
   emoticons_hash
 end
 
@@ -30,6 +37,7 @@ def get_japanese_emoticon(path,emoticon)
   answer_value
 end
 
+<<<<<<< HEAD
 def get_english_meaning(path,emoticon)
   emoticons_hash=load_library(path)
   answer_key="Sorry, that emoticon was not found"
@@ -39,4 +47,8 @@ def get_english_meaning(path,emoticon)
     end
   end
   answer_key
+=======
+def get_english_meaning
+  # code goes here
+>>>>>>> 732e70a4eb0a84688681643024051991d19fd193
 end
